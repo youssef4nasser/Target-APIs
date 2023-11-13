b4a5fa0c8b6c3ebe6824c8445b69f2f910381866
youssef4nasser <youssef4nasser@gmail.com>
2023-11-14 00:49:09 +0200

    add router coupon to bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,6 +4,7 @@ import categoryRouter from "./modules/category/category.routes.js"
 import subCategoryRouter from "./modules/subCategory/subCategory.routes.js"
 import authnRouter from "./modules/Auth/auth.routes.js"
 import { AppError } from "./utils/AppError.js"
+import couponRouter from "./modules/coupon/coupon.routes.js"
 
 export const bootstrap = (app)=>{
     app.get("/", (req, res) => {
@@ -13,6 +14,7 @@ export const bootstrap = (app)=>{
     app.use('/api/v1/brands', brandRouter)
     app.use('/api/v1/categories', categoryRouter)
     app.use('/api/v1/subCategories', subCategoryRouter)
+    app.use('/api/v1/coupons', couponRouter)
     app.all('*', (req, res, next)=>{
         next(new AppError('Not found endpoint', 404))
     })
