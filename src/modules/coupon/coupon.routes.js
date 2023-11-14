a496b82908e1fea0e3ff4c034d874fce99893198
youssef4nasser <youssef4nasser@gmail.com>
2023-11-14 13:22:21 +0200

    coupon.router

--- src/modules/coupon/coupon.routes.js
+++ src/modules/coupon/coupon.routes.js
@@ -1,17 +1,17 @@
 import express from 'express'
-import * as controller from './brand.controller.js'
+import * as controller from './coupon.controller.js'
 import { validate } from '../../middleware/validate.js'
 import { addCouponValidaion, idValidate, updateCouponValidation } from './coupon.validation.js'
 
 const couponRouter = express.Router()
 
 couponRouter.route('/')
-    .post(validate(addCouponValidaion), controller.addBrand)
-    .get(controller.getAllBrands)
+    .post(validate(addCouponValidaion), controller.addCoupon)
+    .get(controller.getAllCoupons)
 
 couponRouter.route('/:id')
-    .get(validate(idValidate), controller.getBrand)
-    .put(validate(updateCouponValidation), controller.updateBrand)
-    .delete(validate(idValidate), controller.deleteBrand)
+    .get(validate(idValidate), controller.getCoupon)
+    .put(validate(updateCouponValidation), controller.updateCoupon)
+    .delete(validate(idValidate), controller.deleteCoupon)
 
 export default couponRouter
